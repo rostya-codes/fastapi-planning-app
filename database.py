e657b6ac6220a082444cc19043fa7ccf9d48f558
rostya <rostyamudrik@proton.me>
2024-07-27 17:20:09 +0300

    Tasks added

---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+engine = create_async_engine(
+    'sqlite+aiosqlite:///tasks.db'
+)
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class TasksOrm(Base):
+    __tablename__ = 'tasks'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[str | None]
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def delete_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/home')
-def get_home():
-    return 'Hello, world!'
+class STaskAdd(BaseModel):
+
+    name: str
+    description: str | None = None
+
+
+class STask(STaskAdd):
+
+    id: int
+
+
+tasks = []
+
+
+@app.post('/tasks')
+async def add_task(
+        task: Annotated[STaskAdd, Depends()]
+):
+    tasks.append(task)
+    return {'ok': True}
+
+
+# @app.get('/tasks')
+# def get_tasks():
+#     task = Task(name='Record this video')
+#     return {'data': task}
