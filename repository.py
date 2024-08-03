ef9fe70da7191c2965484aa46365b21d05ca94ea
rostya <rostyamudrik@proton.me>
2024-08-03 10:40:48 +0300

    STaskId added

--- repository.py
+++ repository.py
@@ -1,14 +1,14 @@
 from sqlalchemy import select
 
 from database import TaskOrm, new_session
-from schemas import STaskAdd
+from schemas import STaskAdd, STask
 
 
 class TaskRepository:
 
     @classmethod
     async def add_one(cls, data: STaskAdd) -> int:
-        async with new_session as session:
+        async with new_session() as session:
             task_dict = data.model_dump()  # Преобразование в словарь
 
             task = TaskOrm(**task_dict)
@@ -18,9 +18,10 @@ class TaskRepository:
             return task.id
 
     @classmethod
-    async def find_all(self):
-        async with new_session as session:
+    async def find_all(self) -> list[STask]:
+        async with new_session() as session:
             query = select(TaskOrm)
             result = await session.execute(query)
             task_models = result.scalars().all()
-            return task_models
+            task_schemas = [STask.model_validate(task_model) for task_model in task_models]
+            return task_schemas
--- router.py
+++ router.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from repository import TaskRepository
-from schemas import STaskAdd
+from schemas import STaskAdd, STask, STaskId
 
 router = APIRouter(
     prefix='/tasks',
@@ -14,12 +14,12 @@ router = APIRouter(
 @router.post('')
 async def add_task(
         task: Annotated[STaskAdd, Depends()]
-):
+) -> STaskId:
     task_id = await TaskRepository.add_one(task)
     return {'ok': True, 'task_id': task_id}
 
 
 @router.get('')
-async def get_tasks():
+async def get_tasks() -> list[STask]:
     tasks = await TaskRepository.find_all()
-    return {'data': tasks}
+    return tasks
